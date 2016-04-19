79c78132788263cf4526045b2fe36f2fd5d8b7ab
Andy Daniel TOMA <andydtoma@hotmail.com>
2016-04-19 18:21:28 +0200

    around build process

--- Bao.Core/Class1.cs
+++ Bao.Core/Class1.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using Microsoft
 
 namespace Bao.Core
 {
     public class Class1
     {
     }
+
+    public class PowerShell : Microsoft.Build.Tasks.Exec
+    {
+        public string ScriptBlock
+        {
+            set
+            {
+                EchoOff = true;
+                Command = string.Format(
+                  "@powershell \"Invoke-Command -ScriptBlock {{ $errorActionPreference='Stop'; {0} ; exit $LASTEXITCODE }} \"",
+                  value.Replace("\"", "\"\"").Replace("\r\n", ";").Replace("\n", ";").Replace("\r", ";"));
+            }
+        }
+    }
 }
+
